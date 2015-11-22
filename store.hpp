6f38581f5b978164863eef98d6b0c001fd649d63
Janusz Lewandowski <lew21@xtreeme.org>
2015-11-22 23:39:05 +0100

    Put store in the xx:: namespace.

--- store.hpp
+++ store.hpp
@@ -1,21 +1,24 @@
 #pragma once
 
-template <class T>
-class store_t
+namespace xx
 {
-	T _v;
-public:
-	store_t() {}
-	store_t(T&& v): _v(std::move(v)) {}
-	T& operator*() {return _v;}
-};
+	template <class T>
+	class store_t
+	{
+		T _v;
+	public:
+		store_t() {}
+		store_t(T&& v): _v(std::move(v)) {}
+		T& operator*() {return _v;}
+	};
 
-template <class T>
-class store_t<T&>
-{
-	T* _v;
-public:
-	store_t() {}
-	store_t(T& v): _v(&v) {}
-	T& operator*() {return *_v;}
-};
+	template <class T>
+	class store_t<T&>
+	{
+		T* _v;
+	public:
+		store_t() {}
+		store_t(T& v): _v(&v) {}
+		T& operator*() {return *_v;}
+	};
+}
